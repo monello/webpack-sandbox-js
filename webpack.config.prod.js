1b6d67834214a325b0e571664ffde2ff7be99129
Morné Louw <louw.morne@gmail.com>
2022-03-18 15:01:25 +0200

    A likely config set up to use

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,33 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: {
-        index: './src/index.js',
-        another: './src/another-module.js',
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
-    },
+    mode: "production",
+    entry: './src/index.js',
     output: {
-        filename: '[name].bundle.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
-    devtool: 'inline-source-map',
     devServer: {
         static: './dist',
-    },
-    optimization: {
-        runtimeChunk: 'single'
     },
     plugins: [
         new HtmlWebpackPlugin({
             hash: true,
             title: 'Development',
-            myPageHeader: 'Hello World - using "webpack.config.js"',
+            myPageHeader: 'Hello World - using "webpack.config.prod.js"',
             template: './src/index.html'
         }),
     ],
