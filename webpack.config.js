3b584c08480ad2fdcb874528f1e2582412c5f4c6
Morné Louw <louw.morne@gmail.com>
2022-03-18 13:30:22 +0200

    Removed the middleware example related changes

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/',
+        clean: true
     },
     devtool: 'inline-source-map',
     devServer: {
