503df51513901cde126e05fdb968a33286f64cf5
Morné Louw <louw.morne@gmail.com>
2022-03-18 16:12:48 +0200

    Aligned the webpack.config.prod.js with the TS setup

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,14 +3,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "production",
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.ts',
+        another: './src/another-module.ts',
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
     devServer: {
         static: './dist',
+    },
+    optimization: {
+        runtimeChunk: 'single'
     },
     plugins: [
         new HtmlWebpackPlugin({
